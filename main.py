7bc46e6bc7455b76a54bd12de414b682aa5ffad5
Anurag-Gour <gour3551@gmail.com>
2022-08-05 22:49:32 +0530

    Upgraded GUI

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
+import tkinter as tk
 from PIL import Image, ImageTk
 from tkinter import filedialog
-
 import os
 
 root = Tk()
@@ -12,6 +12,7 @@ root.geometry("700x310")
 # root.iconify()
 
 root.title("KINESICS")
+root.configure(bg="#F9F5EB") 
 # root.iconbitmap("vsCode.ico")
 
 
@@ -27,31 +28,27 @@ def feedback_form():
 def capture():
     os.startfile('capture.pyw')
 
-
-# BUTTONS
-
-
 f1 = Frame(root)
 
-start_btn = Button(f1, text="START", font="comicsansms", padx="30")
+start_btn = Button(f1, text="START", font="comicsansms", padx="30",border=2,relief=GROOVE)
 start_btn.pack(side="left", padx="65", pady="20")
 
 
 preview_btn = Button(f1, text="PREVIEW", command=capture,
-                     font="comicsansms", padx="20")
+                     font="comicsansms", padx="20",border=2,relief=GROOVE)
 preview_btn.pack(side="left")
 
 
-btn = Button(f1, text="EXIT", command=root.quit, font="comicsansms", padx="30")
+btn = Button(f1, text="EXIT",command=root.quit, font="comicsansms", padx="30",border=2,relief=GROOVE)
 btn.pack(side="left", padx="65", pady="20")
 
 f1.pack(pady=(50, 30))
 
 f2 = Frame(root)
 
-Button(f2, text="COMMANDS", font="comicsansms").pack(side="left",
+Button(f2, text="COMMANDS", font="comicsansms",border=2,relief=GROOVE).pack(side="left",
                                                      padx="35", pady="20")
-Button(f2, text="HELP", font="comicsansms", padx="30").pack(side="left",
+Button(f2, text="HELP", font="comicsansms", padx="30",border=2,relief=GROOVE).pack(side="left",
                                                             padx="55", pady="30")
 
 f2.pack()
